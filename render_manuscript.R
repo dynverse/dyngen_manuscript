e1259538c38a58965e59c31ddb1718b0bb28802d
Robrecht Cannoodt <rcannood@gmail.com>
2019-11-21 10:31:08 +0100

    update rmd and others

--- render_manuscript.R
+++ render_manuscript.R
@@ -10,15 +10,14 @@ if (Sys.info()[["user"]] == "rcannood") {
 # download Rmd from google drive
 httr::set_config(httr::config(http_version = 0)) # avoid http2 framing layer bug
 drive <- drive_download(as_id("1BOltsUJBQ8NhF5s9kU_Q470U0U00QblbG8HBgQwEzRE"), type="text/plain", overwrite=TRUE, path = tempfile())
-system(str_glue("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strange unicode character added by google
-system(str_glue("sed -i 's/ *\\(\\[@[^\\]]*\\]\\)/\\1/' {drive$local_path}")) # remove spaces before citations
-system(str_glue("sed -i 's/^\\(\\#\\#* \\)/\\n\\1/' {drive$local_path}")) # add enters before headings
-system(str_glue("cat {drive$local_path} > manuscript.Rmd"))
+read_lines(drive$local_path) %>%
+  str_replace_all(" *(\\[@[^\\]]*\\])", "\\1") %>% # remove spaces before citations
+  write_lines("manuscript.Rmd")
 
 # render the manuscript
 render("manuscript.Rmd")
 
 # copy content to content.tex
 lines <- read_lines("manuscript.tex")
-content <- lines[seq(which(lines == "\\maketitle")+1, which(lines == "\\printbibliography")-1)]
+content <- lines[seq(which(lines == "\\hypertarget{sec:dyngen-introduction}{%"), which(lines == "\\printbibliography")-1)]
 write_lines(content, "content.tex")

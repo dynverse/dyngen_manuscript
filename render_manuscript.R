1378edf01294a6d62213b94ebc69aad64b356db5
Robrecht Cannoodt <rcannood@gmail.com>
2019-12-01 15:22:22 +0100

    fix render manuscript script

--- render_manuscript.R
+++ render_manuscript.R
@@ -1,16 +1,19 @@
 library(tidyverse)
 library(googledrive)
 library(rmarkdown)
+library(textreadr)
 
 # copy zotero library if on rcannood's computer
 if (Sys.info()[["user"]] == "rcannood") {
   file.copy("~/Workspace/library.bib", "library.bib", overwrite = TRUE)
 }
 
-# download Rmd from google drive
+# download as docx instead of txt, because otherwise comments get pushed into the document
 httr::set_config(httr::config(http_version = 0)) # avoid http2 framing layer bug
-drive <- drive_download(as_id("1BOltsUJBQ8NhF5s9kU_Q470U0U00QblbG8HBgQwEzRE"), type="text/plain", overwrite=TRUE, path = tempfile())
-read_lines(drive$local_path) %>%
+drive <- drive_download(as_id("1BOltsUJBQ8NhF5s9kU_Q470U0U00QblbG8HBgQwEzRE"), type="docx", overwrite=TRUE, path = tempfile())
+
+# read docx
+textreadr::read_docx(drive$local_path, remove.empty = FALSE, trim = FALSE) %>%
   str_replace_all(" *(\\[@[^\\]]*\\])", "\\1") %>% # remove spaces before citations
   write_lines("manuscript.Rmd")
 

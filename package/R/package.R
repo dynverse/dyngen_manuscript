f586722722eed243ebf389e08e51af05043f041f
Robrecht Cannoodt <rcannood@gmail.com>
2019-12-03 10:52:57 +0100

    add helper package

---
+++ package/R/package.R
@@ -0,0 +1,14 @@
+#' Helper package for dyngen manuscript
+#'
+#' @import dplyr
+#' @import tidyr
+#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl discard keep pmap map2 set_names
+#' @importFrom tibble is_tibble as_tibble as_data_frame tibble data_frame enframe deframe lst tribble rownames_to_column column_to_rownames
+#' @importFrom magrittr set_rownames set_colnames
+#' @import assertthat
+#' @import dynutils
+#' @importFrom Matrix t Matrix sparseMatrix summary
+#'
+#' @docType package
+#' @name dyngenanalysis
+NULL

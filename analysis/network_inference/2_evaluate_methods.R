6076a18f96f1ceb24b3de5f64a08a1ff8b14aae2
Robrecht Cannoodt <rcannood@gmail.com>
2019-12-05 11:17:43 +0100

    update results

--- analysis/network_inference/2_evaluate_methods.R
+++ analysis/network_inference/2_evaluate_methods.R
@@ -19,7 +19,7 @@ datasets <- list_as_tibble(map(dataset_files, function(dataset_file) {
 
 methods <- list(
   "bred" = cni_bred(num_trees = 1000L),
-  "SNN*" = cni_deltacor(),
+  "SSN*" = cni_deltacor(),
   "pySCENIC GBM" = cni_pyscenic_sgbm(subsample = 1, n_estimators = 500L),
   "pySCENIC SGBM" = cni_pyscenic_sgbm(subsample = .9, n_estimators = 5000L)
 )
@@ -50,34 +50,38 @@ if (!file.exists(eval_file)) {
   write_rds(lst(evals, aucs), eval_file, compress = "gz")
 }
 
-list2env(read_rds(eval_file))
+list2env(read_rds(eval_file), .GlobalEnv)
+
 aucs <-
   aucs %>%
-  filter(method != "static_casewise") %>%
+  filter(
+    cni_method_id != "bred",
+    method != "static_casewise"
+  ) %>%
   mutate(
     method_label = c("static_static" = "Regular Network Inference", "casewise_casewise" = "Casewise Network Inference")[method],
-    cni_method_id = ifelse(cni_method_id == "deltacor", "SNN*", cni_method_id)
+    cni_method_id = ifelse(cni_method_id == "deltacor", "SSN*", cni_method_id)
   )
 
-ggplot(aucs) + geom_point(aes(auroc, aupr, colour = method)) + facet_wrap(~cni_method_id, nrow = 1)
-ggplot(aucs) + geom_point(aes(auroc, aupr, colour = method)) + facet_grid(cni_method_id~dataset_id)
-ggplot(aucs) + geom_point(aes(auroc, aupr, colour = cni_method_id)) + facet_wrap(~method, scales = "free")
+summ <- aucs %>%
+  group_by(method, method_label, dataset_id, cni_method_id) %>%
+  summarise_if(is.numeric, mean) %>%
+  ungroup()
 
-# g <- ggplot(aucs %>% filter(method == "casewise_casewise")) +
-#   geom_point(aes(auroc, aupr), colour = "lightgray", function(df) select(df, -cni_method_id), size = 1) +
-#   geom_point(aes(auroc, aupr, colour = cni_method_id), size = 1) +
-#   facet_grid(dataset_id~cni_method_id) +
-#   theme_bw()
-# ggsave(paste0("fig/network_inference/comparison_casewise_casewise.pdf"), g, width = 6, height = 15)
-
-
-g <- ggplot(aucs %>% filter(method == "casewise_casewise") %>% sample_n(n())) +
+g <- ggplot(aucs %>% filter(method == "casewise_casewise")) +
   geom_point(aes(auroc, aupr), colour = "lightgray", function(df) select(df, -cni_method_id), size = 1) +
-  geom_point(aes(auroc, aupr, colour = nints), size = 1) +
+  geom_point(aes(auroc, aupr, colour = cni_method_id), size = 1) +
   facet_grid(dataset_id~cni_method_id) +
-  viridis::scale_color_viridis() +
   theme_bw()
-ggsave(paste0("fig/network_inference/comparison_casewise_casewise_nints.pdf"), g, width = 6, height = 15)
+ggsave(paste0("fig/network_inference/comparison_casewise_casewise.pdf"), g, width = 6, height = 15)
+
+# g <- ggplot(aucs %>% filter(method == "casewise_casewise") %>% sample_n(n())) +
+#   geom_point(aes(auroc, aupr), colour = "lightgray", function(df) select(df, -cni_method_id), size = 1) +
+#   geom_point(aes(auroc, aupr, colour = nints), size = 1) +
+#   facet_grid(dataset_id~cni_method_id) +
+#   viridis::scale_color_viridis() +
+#   theme_bw()
+# ggsave(paste0("fig/network_inference/comparison_casewise_casewise_nints.pdf"), g, width = 6, height = 15)
 
 g <- ggplot(aucs %>% filter(method == "static_static")) +
   geom_point(aes(auroc, aupr), colour = "lightgray", function(df) select(df, -cni_method_id), size = 1) +
@@ -86,34 +90,25 @@ g <- ggplot(aucs %>% filter(method == "static_static")) +
   theme_bw()
 ggsave(paste0("fig/network_inference/comparison_static_static.pdf"), g, width = 10, height = 3)
 
-
-
-
-g <- ggplot(aucs %>% filter(method == "casewise_casewise")) +
-  geom_density(aes(F1, colour = cni_method_id)) +
-  facet_wrap(~dataset_id) +
-  theme_bw()
-g
-
-summ <- aucs %>% filter(method == "casewise_casewise") %>% group_by(dataset_id, cni_method_id) %>% summarise_if(is.numeric, mean) %>% ungroup()
-ggplot(summ) +
+ggplot(summ %>% filter(method == "casewise_casewise")) +
   geom_point(aes(auroc, aupr), colour = "gray", function(df) df %>% select(-cni_method_id)) +
   geom_point(aes(auroc, aupr, colour = cni_method_id)) +
   theme_bw() +
   facet_wrap(~cni_method_id)
 
 
-summ <- aucs %>% group_by(dataset_id, cni_method_id, method) %>% summarise_if(is.numeric, mean) %>% ungroup()
-g <- map(c("static_static", "casewise_casewise"), function(meth) {
-  ggplot(summ %>% filter(method == meth)) +
+summplots <- map(c("static_static", "casewise_casewise"), function(meth) {
+  s <- summ %>% filter(method == meth)
+  meth_lab <- s$method_label[[1]]
+  ggplot(s) +
     geom_point(aes(auroc, aupr), colour = "gray", function(df) df %>% select(-cni_method_id)) +
     geom_point(aes(auroc, aupr, colour = cni_method_id)) +
     theme_bw() +
     facet_wrap(~cni_method_id, nrow = 1) +
-    labs(title = meth, x = "mean AUROC", y = "mean AUPR", colour = ifelse(meth == "static_static", "NI method", "CNI method")) +
+    labs(title = meth_lab, x = "mean AUROC", y = "mean AUPR", colour = "Method") +
     scale_color_brewer(palette = "Set1")
-}) %>% patchwork::wrap_plots(ncol = 1)
-ggsave("fig/network_inference/score_summary.pdf", g, width = 8, height = 5)
+})
+ggsave("fig/network_inference/score_summary.pdf", patchwork::wrap_plots(summplots, ncol = 1), width = 8, height = 5)
 
-
+write_rds(summplots, "fig/network_inference/score_summary.rds")
 
--- analysis/network_inference/3_plot_dataset_example.R
+++ analysis/network_inference/3_plot_dataset_example.R
@@ -16,7 +16,7 @@ feature_network <-
   arrange(as.character(regulator) == as.character(target))
 
 # add extra edges invisible between regulators from the same module
-feature_network <-
+feature_network_tmp <-
   bind_rows(
     feature_network,
     feature_info %>%
@@ -31,98 +31,62 @@ feature_network <-
       filter(regulator < target)
   )
 
-gr <- tbl_graph(nodes = feature_info, edges = feature_network)
+gr <- tbl_graph(nodes = feature_info, edges = feature_network_tmp)
 layout <- igraph::layout_with_fr(gr) %>%
   dynutils::scale_minmax() %>%
   magrittr::set_rownames(feature_info$feature_id) %>%
   magrittr::set_colnames(c("x", "y")) %>%
   as.data.frame()
-gr <- gr %>% activate(edges) %>% filter(is.na(effect) | effect != -2)
-feature_network <- feature_network %>% filter(effect != -2)
 
-cap <- circle(1.3, "mm")
-str <- .1
-arrow_up <- grid::arrow(type = "closed", angle = 30, length = grid::unit(1.3, "mm"))
-arrow_down <- grid::arrow(type = "closed", angle = 89, length = grid::unit(1.3, "mm"))
-
-g1 <- ggraph(gr, layout = "manual", x = layout$x, y = layout$y) +
-  geom_edge_loop(aes(strength = str, filter = !is.na(effect) & effect >= 0 & from == to, colour = sign(effect)), arrow = arrow_up, start_cap = cap, end_cap = cap) +
-  geom_edge_loop(aes(strength = str, filter = !is.na(effect) & effect < 0 & from == to, colour = sign(effect)), arrow = arrow_down, start_cap = cap, end_cap = cap) +
-  geom_edge_loop(aes(strength = str, filter = is.na(effect), colour = sign(effect))) +
-  geom_edge_fan(aes(filter = !is.na(effect) & effect >= 0 & from != to, colour = sign(effect)), arrow = arrow_up, start_cap = cap, end_cap = cap) +
-  geom_edge_fan(aes(filter = !is.na(effect) & effect < 0, colour = sign(effect)), arrow = arrow_down, start_cap = cap, end_cap = cap) +
-  geom_edge_fan(aes(filter = is.na(effect))) +
-  geom_node_point(size = 1, colour = "gray") +
-  theme_graph(base_family = "Helvetica") +
-  coord_equal() +
-  labs(size = "is TF", color = "Module group", edge_color = "Regulatory\nactivity") +
-  scale_edge_colour_gradientn(colours = c(rev(RColorBrewer::brewer.pal(5, "Reds")), RColorBrewer::brewer.pal(5, "Greens")))
 
 cell_wps <- do.call(dynwrap::select_waypoint_cells, c(dataset[c("milestone_ids", "milestone_network", "milestone_percentages", "progressions", "divergence_regions")], list(num_cells_selected = 1)))
-cells <- sample(cell_wps, 6) %>% {.[order(match(., dataset$cell_ids))]}
+cells <- sample(cell_wps, 5) %>% {.[order(match(., dataset$cell_ids))]}
 
-g2plots <- map(seq_along(cells), function(i) {
-  cell_id <- cells[[i]]
-  feature_info <- dataset$feature_info
-  feature_network_sc <-
+node_df <- bind_cols(feature_info %>% select(-w:-y), layout)
+
+cap <- .015
+capend <- .02
+edge_df <-
+  bind_rows(
+    feature_network %>% mutate(group = "Global GRN"),
     dataset$regulatory_network_sc %>%
-    filter(cell_id == !!cell_id) %>%
-    arrange(as.character(regulator) == as.character(target)) %>%
-    left_join(feature_network %>% select(regulator, target, effect), by = c("regulator", "target")) %>%
-    mutate(effect = strength)
-  fn <- bind_rows(
-    feature_network_sc,
-    anti_join(feature_network, feature_network_sc, by = c("regulator", "target")) %>% mutate(effect = NA)
+      filter(cell_id %in% cells) %>%
+      arrange(as.character(regulator) == as.character(target)) %>%
+      left_join(feature_network %>% select(regulator, target, effect), by = c("regulator", "target")) %>%
+      mutate(effect = strength, group = paste0("GRN of cell ", match(cell_id, cells)))
   ) %>%
-    select(regulator, target, everything())
-  grsc <- tbl_graph(nodes = feature_info, edges = fn)
-
-
-  # cap <- circle(1, "mm")
-  # str <- .05
-  # arrow_up <- grid::arrow(type = "closed", angle = 30, length = grid::unit(1, "mm"))
-  # arrow_down <- grid::arrow(type = "closed", angle = 89, length = grid::unit(1, "mm"))
-
-  ggraph(grsc, layout = "manual", x = layout$x, y = layout$y) +
-    geom_edge_loop(aes(strength = str, filter = is.na(effect)), colour = "#EEEEEE") +
-    geom_edge_fan(aes(filter = is.na(effect)), colour = "#EEEEEE") +
-    geom_node_point(size = 1, colour = "gray") +
-    geom_edge_loop(aes(strength = str, filter = !is.na(effect) & effect >= 0 & from == to, colour = effect), arrow = arrow_up, start_cap = cap, end_cap = cap) +
-    geom_edge_loop(aes(strength = str, filter = !is.na(effect) & effect < 0 & from == to, colour = effect), arrow = arrow_down, start_cap = cap, end_cap = cap) +
-    geom_edge_fan(aes(filter = !is.na(effect) & effect >= 0 & from != to, colour = effect), arrow = arrow_up, start_cap = cap, end_cap = cap) +
-    geom_edge_fan(aes(filter = !is.na(effect) & effect < 0, colour = effect), arrow = arrow_down, start_cap = cap, end_cap = cap) +
-    theme_graph(base_family = "Helvetica") +
-    coord_equal() +
-    labs(size = "is TF", color = "Module group", title = paste0("GRN of cell ", i)) +
-    scale_edge_colour_gradientn(colours = c(rev(RColorBrewer::brewer.pal(5, "Reds")), RColorBrewer::brewer.pal(5, "Greens"))) +
-    theme(legend.position = "none")
-})
-#
-# g <- patchwork::wrap_plots(g1 + labs(title = "Global GRN"), patchwork::wrap_plots(g2plots, nrow = 2), nrow = 1, widths = c(2, 3.5)) &
-#   theme(
-#     plot.margin = margin(0, 0, 0, 0, "cm"),
-#     plot.title = element_text(size = 10),
-#     legend.text = element_text(size = 6),
-#     legend.title = element_text(size = 8)
-#   )
-# ggsave("fig/network_inference/casewise_grn.pdf", width = 11, height = 4)
-
-
-
-g <- patchwork::wrap_plots(
-  c(
-    list(g1 + labs(title = "Global GRN") + theme(legend.position = "none")),
-    g2plots[1:2],
-    list(ggpubr::as_ggplot(ggpubr::get_legend(g1 + theme(legend.text = element_text(size = 6), legend.title = element_text(size = 8))))),
-    g2plots[3:5],
-    list(patchwork::plot_spacer())
-  ),
-  nrow = 2,
-  widths = c(3, 3, 3, .75)
-) &
-  theme(
-    plot.margin = margin(0, 0, 0, 0, "cm"),
-    plot.title = element_text(size = 10, hjust = .5)
+  mutate(
+    regulator = as.character(regulator),
+    target = as.character(target),
+    x_ = layout[regulator, "x"],
+    y_ = layout[regulator, "y"],
+    xend_ = layout[target, "x"],
+    yend_ = layout[target, "y"],
+    len = sqrt( (x_ - xend_)^2 + (y_ - yend_)^2 ),
+    al = (len - cap) / len,
+    alend = (len - capend) / len,
+    x = al * x_ + (1 - al) * xend_,
+    y = al * y_ + (1 - al) * yend_,
+    xend = alend * xend_ + (1 - alend) * x_,
+    yend = alend * yend_ + (1 - alend) * y_
   )
-ggsave("fig/network_inference/casewise_grn.pdf", width = 8, height = 6)
+
+arrow_up <- grid::arrow(type = "closed", angle = 30, length = grid::unit(1.3, "mm"))
+arrow_down <- grid::arrow(type = "closed", angle = 89, length = grid::unit(1.3, "mm"))
+g <- ggplot() +
+  # geom_segment(aes(x = x, y = y, xend = xend, yend = yend), edge_df %>% filter(is.na(cell_id)) %>% select(-group), colour = "lightgray") +
+  geom_point(aes(x, y), colour = "gray", node_df) +
+  geom_segment(aes(x = x, y = y, xend = xend, yend = yend, colour = effect), edge_df %>% filter(effect >= 0), arrow = arrow_up) +
+  geom_segment(aes(x = x, y = y, xend = xend, yend = yend, colour = effect), edge_df %>% filter(effect < 0), arrow = arrow_down) +
+  theme_graph(base_family = "Helvetica") +
+  coord_equal() +
+  labs(colour = "Regulatory\nactivity") +
+  scale_colour_gradientn(colours = c(rev(RColorBrewer::brewer.pal(5, "Reds")), RColorBrewer::brewer.pal(5, "Greens"))) +
+  facet_wrap(~group) +
+  theme(
+    plot.margin = margin(0, 0, 0, 0, "cm")
+  )
+
+ggsave("fig/network_inference/casewise_grn.pdf", g, width = 8, height = 5)
+write_rds(g, "fig/network_inference/casewise_grn.rds")
 

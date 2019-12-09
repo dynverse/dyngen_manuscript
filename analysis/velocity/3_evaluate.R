93b3301100257c945149b71d717e16b1f3111e29
Wouter Saelens <wouter.saelens@gmail.com>
2019-12-09 12:34:10 +0100

    update evlauate

--- analysis/velocity/3_evaluate.R
+++ analysis/velocity/3_evaluate.R
@@ -84,10 +84,12 @@ plot_velocity_gs <- function(
     )
   }
 
+  labels <- setNames(LETTERS[seq_along(dataset$milestone_ids)], dataset$milestone_ids)
   plot_trajectory <- dynplot_dimred(dataset) +
     geom_cell_point(aes(color = milestone_percentages)) +
     scale_milestones_fillcolour() +
     geom_trajectory_segments(size = 2, color = "#333333") +
+    geom_milestone_label(aes(fill = milestone_id, label = labels[label]), color = "black") +
     ggtitle(method_id) +
     theme_common(legend.position = "none") +
     ggtitle("Trajectory")
@@ -216,6 +218,10 @@ tag_first <- function(x, tag) {
   x
 }
 
+plot_scores <- plot_scores + labs(tag = "A")
+plot_velocity_gs[[1]][[1]] <- plot_velocity_gs[[1]][[1]] + labs(tag = "B")
+plot_velocity_gs[[2]][[1]] <- plot_velocity_gs[[2]][[1]] + labs(tag = "C")
+plot_velocity_arrows[[1]] <- plot_velocity_arrows[[1]] + labs(tag = "D")
 
 pdf("results/velocity/usecase.pdf", height = 14, width = 10, useDingbats = F)
 patchwork::wrap_plots(
@@ -224,7 +230,7 @@ patchwork::wrap_plots(
   (plot_velocity_arrows & theme(plot.title = element_blank(), plot.subtitle = element_blank())),
   ncol = 1,
   heights = c(1, 2, 1)
-) + patchwork::plot_annotation(tag_levels = c("A"))
+)
 graphics.off()
 
 magick::image_read_pdf("results/velocity/usecase.pdf") %>%
@@ -232,131 +238,3 @@ magick::image_read_pdf("results/velocity/usecase.pdf") %>%
   magick::image_write("results/velocity/usecase.png") %>%
   invisible()
 
-
-
-
-
-
-
-
-
-
-compare_velocity_correlation <- function(model, dataset, velocity, ...) {
-  model <- load_model(dataset_id)
-  dataset <- load_dataset_velocity(dataset_id, method_id, params_id)
-  groundtruth_velocity <- extract_groundtruth_velocity(model)
-
-  velocity_differences <- dataset$expression_projected - dataset$expression
-  velocity_differences[velocity_differences == 0] <- NA
-  velocity_differences[is.na(velocity_differences)] <- runif(sum(is.na(velocity_differences)), -1e-10, 1e-10)
-
-  plot(log2(groundtruth_velocity[, feature_id] %>% pmin(100) %>% pmax(0.01)), velocity_differences[, feature_id])
-}
-
-design_velocity %>% mutate(
-  model = load_model(dataset_id),
-  dataset = load_dataset(dataset_id),
-  velocity = load_velocity(dataset_id, method_id, params_id)
-)
-
-
-design_velocity %>% filter(dataset_id == "1") %>% pmap(compare_velocity_correlation)
-
-
-
-
-
-
-
-
-
-plot_velocity_grid <- function(
-  design_velocity_oi,
-  dataset = load_dataset(design_velocity$dataset_id[[1]])
-) {
-  dataset <- dataset %>% add_dimred(dyndimred::dimred_landmark_mds, pair_with_velocity = FALSE)
-
-  velocity_plots <- pmap(design_velocity_oi, function(dataset_id, method_id, params_id, ...) {
-    velocity <- load_velocity(dataset_id, method_id, params_id)
-    dataset <- dataset %>% add_velocity(velocity = velocity)
-    dataset <- dataset %>%
-      add_dimred(dimred = dataset$dimred, dimred_projected = embed_velocity(velocity$scvelo, dataset$dimred))
-
-    dynplot_dimred(dataset) +
-      geom_cell_point(color = "grey") +
-      geom_velocity_arrow(size = 1) +
-      ggtitle(paste0(method_id, " ", params_id))
-  })
-
-  patchwork::wrap_plots(
-    c(
-      velocity_plots
-    )
-  )
-}
-
-plot_velocity_grid(design_velocity_oi)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-groundtruth_velocity <- extract_groundtruth_velocity(model)
-
-
-plot(log2(groundtruth_velocity[, feature_id] %>% pmin(10) %>% pmax(0.1)), velocity_differences[, feature_id])
-
-
-assertthat::assert_that(all(rownames(groundtruth_velocity) == rownames(velocity_differences)))
-assertthat::assert_that(all(colnames(groundtruth_velocity) == colnames(velocity_differences)))
-
-cors <- pairwise_correlations(
-  Matrix::t(velocity_differences),
-  t(groundtruth_velocity)
-)
-
-
-dataset <- dataset %>% add_dimred(dyndimred::dimred_landmark_mds, pair_with_velocity = FALSE)
-dataset <- add_velocity(dataset, velocity = velocity)
-
-qplot(cors)
-
-feature_id <- find_updown_feature(model)[10]
-feature_id <- dataset$feature_ids[10]
-dynplot_dimred(dataset) +
-  geom_cell_point(aes(color = pmin(log(groundtruth_velocity[,feature_id]), 5))) +
-  scale_color_viridis_c()
-dynplot_dimred(dataset) +
-  geom_cell_point(aes(color = select_feature_expression(feature_id, .data))) +
-  scale_color_viridis_c()
-dynplot_dimred(dataset) +
-  geom_cell_point(aes(color = select_feature_velocity(feature_id, .data))) +
-  scale_color_viridis_c()

9956b322ea7508f7e888f5c0fe23d5f26eef76f5
Wouter Saelens <wouter.saelens@gmail.com>
2020-05-04 12:00:36 +0200

    fix rna velocity parameters

--- package/R/rnav_run_velocyto.R
+++ package/R/rnav_run_velocyto.R
@@ -49,7 +49,7 @@ rnav_run_velocyto <- function(
   vlm$`_normalize_S`(relative_size=rowSums(vlm$S), target_size=mean(rowSums(vlm$S)))
   vlm$`_normalize_U`(relative_size=rowSums(vlm$U), target_size=mean(rowSums(vlm$U)))
   vlm$perform_PCA()
-  vlm$knn_imputation(n_pca_dims=20L, pca_space = TRUE, k=20L, balanced=TRUE, b_sight=50L, b_maxl=20L, n_jobs=16L, size_norm=FALSE)
+  vlm$knn_imputation(n_pca_dims=20L, k=20L, balanced=TRUE, b_sight=200L, b_maxl=500L, n_jobs=16L)
   vlm$fit_gammas()
   vlm$predict_U()
   vlm$calculate_velocity()

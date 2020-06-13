80427a749ac539fedf34e72e42c76144639b0d30
Robrecht Cannoodt <rcannood@gmail.com>
2020-06-13 19:00:35 +0200

    update

--- render_manuscript.R
+++ render_manuscript.R
@@ -11,11 +11,12 @@ drive <- drive_download(as_id("15ZkzzB-XSYFfZutO4YPkUIw6iUvxJS92fSPfzqR1cLA"), t
 textreadr::read_docx(drive$local_path, remove.empty = FALSE, trim = FALSE) %>%
   str_replace_all("([^ ])(\\[@[^\\]]*\\])", "\\1 \\2") %>% # add spaces before citations
   str_replace_all("^#", "\n#") %>%
-  write_lines("manuscript.Rmd")
+  write_lines("render.Rmd")
 
 # render the manuscript
-render("manuscript.Rmd")
+render("render.Rmd")
 
-system("pdftk manuscript.pdf cat 1-17 output manu.pdf")
-system("pdftk manuscript.pdf cat 18-25 output supp.pdf")
+system("pdftk render.pdf cat 1-17 output manuscript.pdf")
+system("pdftk render.pdf cat 18-25 output supplementary_files.pdf")
 
+file.remove("render.pdf")
